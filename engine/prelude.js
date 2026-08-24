2d2d9f8d5fe510d80065a954752d7784b586c2ff
snicker02 <snicker02@gmail.com>
2026-08-23 21:43:46 -0400

    Update prelude.js

--- engine/prelude.js
+++ engine/prelude.js
@@ -8,7 +8,7 @@
 //   operator norm of its Jacobian at p. The estimator finishes with prim(p) / s.
 //   Under-report s and the ray punches through surfaces. Over-report and you only lose speed.
 
-export const BUILD = '0.24.0-xform-cap';
+export const BUILD = '0.25.0-plane-order';
 
 export const VS = `#version 300 es
 in vec2 aPos;
